96ac44876d06b77e4dc1515e628d91c08ebc869f
KristianPopov <kristian@popovbg.eu>
2014-06-01 23:55:26 +0300

    Update GL_Rendering.h

--- GL_Rendering.h
+++ GL_Rendering.h
@@ -6,19 +6,19 @@
 #define GL_TEXTURE_MAX_ANISOTROPY_EXT     0x84FE
 #define GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT 0x84FF
 
-bool SAD_Engine_Advert(GLuint powered_tex,int timer){
+bool SAD_Engine_Advert(GLuint powered_tex,int timer,bool on){
      
-  if(timer<8000){
+  if((timer<8000)&&(on)){
     glBegin(GL_QUADS);
-    glColor4f(0.0f, 0.0f, 0.0f, (3000-timer)*0.0005); 
+    glColor4f(0.0f, 0.0f, 0.0f, (3000-timer)*0.0005);
     glTexCoord2f(0.0, 1.0);
-    glVertex3f(-1.3f, -1.0f, -1);
+    glVertex3f(-1.8f, -1.0f, -0.1);
     glTexCoord2f(1.0, 1.0);
-    glVertex3f(1.3f, -1.0f, -1);
+    glVertex3f(1.8f, -1.0f, -0.1);
     glTexCoord2f(1.0, 0.0);
-    glVertex3f(1.3f, 1.0f,  -1);
+    glVertex3f(1.8f, 1.0f,  -0.1);
     glTexCoord2f(0.0, 0.0);
-    glVertex3f(-1.3f, 1.0f, -1);
+    glVertex3f(-1.8f, 1.0f, -0.1);
     glEnd();
     
     glEnable(GL_TEXTURE_2D);
@@ -26,29 +26,29 @@ bool SAD_Engine_Advert(GLuint powered_tex,int timer){
     glBegin(GL_QUADS);
     glColor4f(1.0f, 1.0f, 1.0f, 1.0); 
     glTexCoord2f(0.0, 1.0);
-    glVertex3f(-1.3f, -1.0f, -1);
+    glVertex3f(-1.333f, -1.0f, -0.1);
     glTexCoord2f(1.0, 1.0);
-    glVertex3f(1.3f, -1.0f, -1);
+    glVertex3f(1.333f, -1.0f, -0.1);
     glTexCoord2f(1.0, 0.0);
-    glVertex3f(1.3f, 1.0f,  -1);
+    glVertex3f(1.333f, 1.0f,  -0.1);
     glTexCoord2f(0.0, 0.0);
-    glVertex3f(-1.3f, 1.0f, -1);
+    glVertex3f(-1.333f, 1.0f, -0.1);
     glEnd();
     glDisable(GL_TEXTURE_2D);
     return true;
   }
   else{
-    PlaySound("Sounds/night_hunter.wav", NULL, SND_ASYNC|SND_LOOP);
+    PlaySound("Sounds/menu.wav", NULL, SND_ASYNC|SND_LOOP);
     return false;
   }
 }
 
-void GL_init(){
+void GL_init(unsigned int res_x, unsigned int res_y){
      GL2Init();
-     glViewport(0, 0, 640, 480);
+     glViewport(0, 0, res_x, res_y);
      glMatrixMode(GL_PROJECTION);
      glLoadIdentity();
-     gluPerspective(40.0f,(GLfloat)640/(GLfloat)480,0.0001f,10000000.0f);
+     gluPerspective(40.0f,(GLfloat)res_x/(GLfloat)res_y,0.0001f,10000000.0f);
      glMatrixMode(GL_MODELVIEW);
      glLoadIdentity();
      glEnable(GL_DEPTH_TEST);
@@ -130,7 +130,7 @@ void RenderProjectile(projectile* proj,camera cam,GLuint proj_tex,int count_proj
     }              
 }
 
-void RenderModel(int num_p, float* arr_v,float* arr_vt, float* arr_vn,int* arr_f,GLuint texture, GLuint texture1, GLuint texture2){
+void RenderModel(int num_p, float* arr_v,float* arr_vt, float* arr_vn,int* arr_f,GLuint texture, GLuint texture1, GLuint texture2,int tex1_start, int tex2_start){
      glPushMatrix();
      glLineWidth(1);
      glTranslatef(0,0.01,0);
@@ -138,7 +138,7 @@ void RenderModel(int num_p, float* arr_v,float* arr_vt, float* arr_vn,int* arr_f
      glBindTexture(GL_TEXTURE_2D, texture);
      glBegin(GL_TRIANGLES);
      int i=0;
-     while(i<num_p-90){
+     while(i<num_p-tex1_start){
          glNormal3d(arr_vn[(arr_f[i+2]-1)*3], arr_vn[(arr_f[i+2]-1)*3+1], arr_vn[(arr_f[i+2]-1)*3+2]);
          glTexCoord2f(arr_vt[(arr_f[i+1]-1)*2], arr_vt[(arr_f[i+1]-1)*2+1]);
          glVertex3f(arr_v[(arr_f[i]-1)*3], arr_v[(arr_f[i]-1)*3+1], arr_v[(arr_f[i]-1)*3+2]);
@@ -148,7 +148,7 @@ void RenderModel(int num_p, float* arr_v,float* arr_vt, float* arr_vn,int* arr_f
      
      glBindTexture(GL_TEXTURE_2D, texture1);     
      glBegin(GL_TRIANGLES);
-     while(i<num_p-27){
+     while(i<num_p-tex2_start){
          glNormal3d(arr_vn[(arr_f[i+2]-1)*3], arr_vn[(arr_f[i+2]-1)*3+1], arr_vn[(arr_f[i+2]-1)*3+2]);
          glTexCoord2f(arr_vt[(arr_f[i+1]-1)*2], arr_vt[(arr_f[i+1]-1)*2+1]);
          glVertex3f(arr_v[(arr_f[i]-1)*3], arr_v[(arr_f[i]-1)*3+1], arr_v[(arr_f[i]-1)*3+2]);
@@ -179,6 +179,82 @@ void RenderModel(int num_p, float* arr_v,float* arr_vt, float* arr_vn,int* arr_f
      //glDrawArrays(GL_TRIANGLE_FAN,0,(num_p-1)/3);
      //glDrawRangeElements(GL_LINE_LOOP, 0, 3, 4, GL_UNSIGNED_BYTE, arr_f);
      //glDrawElements(GL_TRIANGLE_FAN, num_p, GL_UNSIGNED_BYTE, arr_f);
+     glDisable(GL_TEXTURE_2D);
+     glPopMatrix();          
+}
+
+
+void RenderModel(float x,float y,float z,model model3D,GLuint texture,
+                       GLuint texture1, GLuint texture2, GLuint texture3, GLuint texture4,
+                       int tex1_start, int tex2_start, int tex3_start, int tex4_start){
+     glPushMatrix();
+     glLineWidth(1);
+     glTranslatef(x,y,z);
+     glEnable(GL_TEXTURE_2D);
+     glBindTexture(GL_TEXTURE_2D, texture);
+     glBegin(GL_TRIANGLES);
+     int i=0;
+     while(i<model3D.num_f-tex1_start){
+         glNormal3d(model3D.arr_vn[(model3D.arr_f[i+2]-1)*3], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+1], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+2]);
+         glTexCoord2f(model3D.arr_vt[(model3D.arr_f[i+1]-1)*2], model3D.arr_vt[(model3D.arr_f[i+1]-1)*2+1]);
+         glVertex3f(model3D.arr_v[(model3D.arr_f[i]-1)*3], model3D.arr_v[(model3D.arr_f[i]-1)*3+1], model3D.arr_v[(model3D.arr_f[i]-1)*3+2]);
+         i+=3;
+     }
+     glEnd();
+     
+      glBindTexture(GL_TEXTURE_2D, texture1);     
+     glBegin(GL_TRIANGLES);
+     while(i<model3D.num_f-tex2_start){
+         glNormal3d(model3D.arr_vn[(model3D.arr_f[i+2]-1)*3], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+1], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+2]);
+         glTexCoord2f(model3D.arr_vt[(model3D.arr_f[i+1]-1)*2], model3D.arr_vt[(model3D.arr_f[i+1]-1)*2+1]);
+         glVertex3f(model3D.arr_v[(model3D.arr_f[i]-1)*3], model3D.arr_v[(model3D.arr_f[i]-1)*3+1], model3D.arr_v[(model3D.arr_f[i]-1)*3+2]);
+         i+=3;
+     }
+     glEnd();
+ 
+     glBindTexture(GL_TEXTURE_2D, texture2);     
+     glBegin(GL_TRIANGLES);    
+     while(i<model3D.num_f-tex3_start){
+         glNormal3d(model3D.arr_vn[(model3D.arr_f[i+2]-1)*3], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+1], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+2]);
+         glTexCoord2f(model3D.arr_vt[(model3D.arr_f[i+1]-1)*2], model3D.arr_vt[(model3D.arr_f[i+1]-1)*2+1]);
+         glVertex3f(model3D.arr_v[(model3D.arr_f[i]-1)*3], model3D.arr_v[(model3D.arr_f[i]-1)*3+1], model3D.arr_v[(model3D.arr_f[i]-1)*3+2]);
+         i+=3;
+     }
+     glEnd();
+     
+     glBindTexture(GL_TEXTURE_2D, texture3);     
+     glBegin(GL_TRIANGLES);    
+     while(i<model3D.num_f-tex4_start){
+         glNormal3d(model3D.arr_vn[(model3D.arr_f[i+2]-1)*3], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+1], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+2]);
+         glTexCoord2f(model3D.arr_vt[(model3D.arr_f[i+1]-1)*2], model3D.arr_vt[(model3D.arr_f[i+1]-1)*2+1]);
+         glVertex3f(model3D.arr_v[(model3D.arr_f[i]-1)*3], model3D.arr_v[(model3D.arr_f[i]-1)*3+1], model3D.arr_v[(model3D.arr_f[i]-1)*3+2]);
+         i+=3;
+     }
+     glEnd(); 
+     
+     glBindTexture(GL_TEXTURE_2D, texture4);     
+     glBegin(GL_TRIANGLES);
+     while(i<model3D.num_f){
+         glNormal3d(model3D.arr_vn[(model3D.arr_f[i+2]-1)*3], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+1], model3D.arr_vn[(model3D.arr_f[i+2]-1)*3+2]);
+         glTexCoord2f(model3D.arr_vt[(model3D.arr_f[i+1]-1)*2], model3D.arr_vt[(model3D.arr_f[i+1]-1)*2+1]);
+         glVertex3f(model3D.arr_v[(model3D.arr_f[i]-1)*3], model3D.arr_v[(model3D.arr_f[i]-1)*3+1], model3D.arr_v[(model3D.arr_f[i]-1)*3+2]);
+         i+=3;
+     }
+     glEnd();
+     
+     glDisableClientState (GL_COLOR_ARRAY);
+     
+/*     glPointSize(10);
+     
+     glVertexPointer (3, GL_FLOAT, 0, arr_v);    
+     glNormalPointer(GL_FLOAT, 0, arr_vn);
+     glTexCoordPointer(2, GL_FLOAT, 0, arr_vt);
+     glColor4f(0,0.5,1,1);
+     //glDrawArrays(GL_POINTS,1,num_p/3);
+     glColor4f(1,1,1,1);
+     //glDrawArrays(GL_TRIANGLE_FAN,0,(num_p-1)/3);
+     //glDrawRangeElements(GL_LINE_LOOP, 0, 3, 4, GL_UNSIGNED_BYTE, arr_f);
+     //glDrawElements(GL_TRIANGLE_FAN, num_p, GL_UNSIGNED_BYTE, arr_f);*/
      glDisable(GL_TEXTURE_2D);
      glPopMatrix();          
 }
@@ -447,3 +523,123 @@ void RenderSkybox(GLuint skybox[5]){
     glEnable(GL_LIGHTING);
     glPopMatrix();
 }     
+
+void RenderCursor(float x,float y,GLuint cursor){
+    glPushMatrix ();
+    glBindTexture(GL_TEXTURE_2D, cursor);
+    glTranslatef(x,y,0);
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_QUADS);
+    glColor4f(1,1,1,1);           
+    glTexCoord2f(0.0, 1.0);
+    glVertex3f(-0.1, -0.1, -0.11);
+    glTexCoord2f(1.0, 1.0);
+    glVertex3f(0.1, -0.1, -0.11);
+    glTexCoord2f(1.0, 0.0);
+    glVertex3f(0.1, 0.1, -0.11);
+    glTexCoord2f(0.0, 0.0);
+    glVertex3f(-0.1, 0.1, -0.11);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix ();      
+}
+
+void RenderButton(button btn,GLuint off_tex,GLuint on_tex,int tone){
+    glPushMatrix();
+    glTranslatef(btn.get_x(),btn.get_y(),0);
+    glEnable(GL_TEXTURE_2D);
+    if(btn.selected())glBindTexture(GL_TEXTURE_2D, on_tex);
+    else glBindTexture(GL_TEXTURE_2D, off_tex);
+    glBegin(GL_QUADS);
+    glColor4f(cos(tone*PI/180), cos(tone*PI/180), cos(tone*PI/180), 1.0f); 
+    glTexCoord2f(0.0, 1.0);
+    glVertex3f(0, btn.get_height(), -0.11);
+    glTexCoord2f(1.0, 1.0);
+    glVertex3f(btn.get_width(), btn.get_height(), -0.11);
+    glTexCoord2f(1.0, 0.0);
+    glVertex3f(btn.get_width(), 0,  -0.11);
+    glTexCoord2f(0.0, 0.0);
+    glVertex3f(0, 0, -0.11);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
+}
+
+void RenderButton(button btn,float r, float g, float b, float a ,float s_r, float s_g, float s_b, float s_a){
+    glPushMatrix();
+    glTranslatef(btn.get_x(),btn.get_y(),0);
+    glDisable(GL_TEXTURE_2D);
+    if(btn.selected()) glColor4f(s_r, s_g, s_b, s_a); 
+    else glColor4f(r, g, b, a);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 1.0);
+    glVertex3f(0, btn.get_height(), -0.11);
+    glTexCoord2f(1.0, 1.0);
+    glVertex3f(btn.get_width(), btn.get_height(), -0.11);
+    glTexCoord2f(1.0, 0.0);
+    glVertex3f(btn.get_width(), 0,  -0.11);
+    glTexCoord2f(0.0, 0.0);
+    glVertex3f(0, 0, -0.11);
+    glEnd();
+    glPopMatrix();
+}
+
+void RenderScreenWithColor(float r, float g, float b, float a){
+    glBegin(GL_QUADS);
+    glColor4f(r, g, b, a); 
+    glVertex3f(-1.8f, -1.0f, -0.11);
+    glVertex3f(1.8f, -1.0f, -0.11);
+    glVertex3f(1.8f, 1.0f,  -0.11);
+    glVertex3f(-1.8f, 1.0f, -0.11);
+    glEnd();     
+}
+
+void WriteText (float tx, 
+               float ty,
+               float ts,
+               CHAR* SText,
+               float tr,
+               float tg,
+               float tb,
+               float ta,
+               CHAR* SType,
+               GLuint font_tex)
+{
+                          
+    //char SFCHR[1024];
+    int STcnt=0;
+    int U,V=0;
+    //STlnt=strlen(SText); 
+    glPushMatrix ();          
+    glScalef(ts,ts,1);
+    glTranslatef(tx/ts,ty/ts,0);
+    //strcpy(SFCHR, SText);
+    glBindTexture(GL_TEXTURE_2D, font_tex);
+    glEnable(GL_TEXTURE_2D);
+   while (SText[STcnt]!='\0') 
+   {
+    V=((int)SText[STcnt]/16)-1;
+    U=(int)SText[STcnt]-(V*16)-1;
+    if (U!=15) V++;
+    if (SType=="Vertical") {ty=tx; tx=0;}
+    if (SType=="Diagonal//") ty=tx;
+    if (SType=="Diagonal\\") ty=-tx;
+    glTranslatef(tx,ty,0);
+    if ((SType=="Defaut") or (SType!="Diagonal//") or (SType!="Diagonal\\") or (SType==NULL) or (SType!="Vertical")) ty=0;     
+    glBegin(GL_QUADS);
+     glColor4f(tr,tg,tb,ta);           
+     glTexCoord2f(U*0.0625, 0.064+V*0.0625);
+     glVertex3f(-0.1, -0.1, -0.11);
+     glTexCoord2f(0.064+U*0.0625, 0.064+V*0.0625);
+     glVertex3f(0.1, -0.1, -0.11);
+     glTexCoord2f(0.064+U*0.0625, V*0.0625);
+     glVertex3f(0.1, 0.1, -0.11);
+     glTexCoord2f(U*0.0625, V*0.0625);
+     glVertex3f(-0.1, 0.1, -0.11);
+    glEnd();
+    STcnt++;
+    tx=+0.2;
+    } 
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix();  
+}
